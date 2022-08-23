daaa1425efa9eec635b8639a464708f1b01964ea
benber86 <benoitb@tutanota.de>
2022-08-23 16:05:36 +1000

    Schedule multiple jobs per chain

--- app/tasks/populate.py
+++ app/tasks/populate.py
@@ -1,7 +1,12 @@
 from app import celery
 from tasks.queries.curve.pools import get_curve_pools
+#from tasks.database.curve.pools import update_curve_pools
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 
 @celery.task
-def populate_db():
-    get_curve_pools("mainnet")
+def populate_curve_pools(chain):
+    logger.info(f"Updating Curve Pools for {chain}")
+    #update_curve_pools(get_curve_pools(chain))
