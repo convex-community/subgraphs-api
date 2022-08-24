2ca40b36fd976ef57014c8353aaf2e053e7f2b02
benber86 <benoitb@tutanota.de>
2022-08-24 14:35:13 +1000

    Add retries on graph queries

--- app/tasks/queries/graph.py
+++ app/tasks/queries/graph.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from typing import List, Mapping, Any, Optional
 from celery.utils.log import get_task_logger
 from app import celery
@@ -7,10 +8,16 @@ logger = get_task_logger(__name__)
 
 
 def grt_query(chain: str, query: str) -> Optional[Mapping[str, List[Mapping[str, Any]]]]:
-    endpoint_mapping = celery.conf.get("SUBGRAPHS")
-    endpoint = endpoint_mapping.get(chain, None)
-    if endpoint is None:
-        logger.warning(f"Unable to find an endpoint for chain {chain}")
-        return None
-    r = requests.post(endpoint, json={'query': query})
-    return r.json()['data']
+    for i in range(3):
+        endpoint_mapping = celery.conf.get("SUBGRAPHS")
+        endpoint = endpoint_mapping.get(chain, None)
+        if endpoint is None:
+            logger.warning(f"Unable to find an endpoint for chain {chain}")
+            return None
+        r = requests.post(endpoint, json={'query': query})
+        try:
+            return r.json()['data']
+        except (requests.exceptions.RequestsJSONDecodeError, requests.exceptions.ConnectionError):
+            logger.error(f"Failed at fulfilling request {query} for {chain}, retrying (f{i}/3)")
+            time.sleep(60)
+            continue
