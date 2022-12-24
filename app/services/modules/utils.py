5042db4bde471058fa1e5c18d53a6a2372c0b3cf
benber86 <benoitb@tutanota.de>
2022-12-24 23:41:06 +1100

    Fix LIMIT and OFFSET since both args need to be present in query

--- app/services/modules/utils.py
+++ app/services/modules/utils.py
@@ -4,8 +4,7 @@ from typing import Optional
 def append_offset_and_limit(
     query: str, offset: Optional[int] = None, limit: Optional[int] = None
 ) -> str:
-    if offset:
-        query += f" OFFSET {offset}"
-    if limit:
-        query += f" LIMIT {limit}"
+    if offset or limit:
+        query += f" OFFSET {offset if offset else 0}"
+        query += f" LIMIT {limit if limit else 1}"
     return query
