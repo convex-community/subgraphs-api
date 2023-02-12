0f7559568e10a8dd27adfa18bbee391ec67e3442
benber86 <benoitb@tutanota.de>
2023-02-13 07:17:04 +1100

    Update dev subgraph endpoint

--- app/main/const/subgraphs.py
+++ app/main/const/subgraphs.py
@@ -50,8 +50,7 @@ SUBGRAPH_XDAI_API_DEV = (
 
 CONVEX_POOLS_SUBGRAPH_PROD = f"https://gateway.thegraph.com/api/{SUBGRAPHS_API_KEY}/subgraphs/id/6x6tQirWcrrESPPQU5cRkbtZnGBEAKHZrpJ7he9xqeth"
 CONVEX_POOLS_SUBGRAPH_DEV = (
-    "https://api.thegraph.com/subgraphs/id/QmNT9P1DTBBtrPxWhDAZPJgrtQPTej5uFFBv9dkfo4ZGRm"
-    # "https://api.thegraph.com/subgraphs/name/convex-community/curve-pools"
+    "https://thegraph.com/hosted-service/subgraph/convex-community/convex"
 )
 
 CURVE_SUBGRAPHS_PROD = {
