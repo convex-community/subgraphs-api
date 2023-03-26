4b2308c39fa639e286e2a332453b9ed7991a76ab
benber86 <benoitb@tutanota.de>
2023-03-26 17:00:42 +1100

    Ignore type

--- app/tasks/database/convex/snapshots.py
+++ app/tasks/database/convex/snapshots.py
@@ -11,7 +11,7 @@ CONTAINER_NAME = "ConvexPoolSnapshots"
 def update_convex_pool_snapshots(pools: List[ConvexPoolSnapshot]):
     container = get_container(CONTAINER_NAME)
     for pool in pools:
-        base_apr = pool.baseApr if "baseApr" in pool.__dict__ else None
+        base_apr = pool["baseApr"] if "baseApr" in pool else None  # type: ignore
         if base_apr is None or math.isnan(base_apr) or math.isinf(base_apr):
             logger.error(f"Invalid or inexistant value for baseApr in {pool}")
             continue
