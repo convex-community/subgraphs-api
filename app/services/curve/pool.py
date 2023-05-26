6a79d9751d9068e37de2e41f9d56fb745645cfa7
benber86 <benoitb@tutanota.de>
2023-05-26 22:52:48 +1000

    Move Snapshot queries to postgres

--- app/services/curve/pool.py
+++ app/services/curve/pool.py
@@ -7,30 +7,37 @@ from models.curve.pool import (
     CurvePoolNameChain,
 )
 from main import db
-from typing import List, Mapping
+from typing import List
 from marshmallow import EXCLUDE
 
 
 def get_all_pool_names() -> List[CurvePoolNameChain]:
-    result = db.query(CurvePool.address, CurvePool.chain, CurvePool.name).all()
-    return CurvePoolNameChainSchema(many=True, session=db.session).load(
-        result, unknown=EXCLUDE
+    result = (
+        db.session.query(CurvePool)
+        .with_entities(CurvePool.address, CurvePool.chain, CurvePool.name)
+        .all()
     )
+
+    result = [CurvePoolNameChainSchema().load(row._asdict()) for row in result]
+
+    return result
 
 
 def get_pool_names(chain: str) -> List[CurvePoolName]:
     result = (
-        db.query(CurvePool.address, CurvePool.name)
+        db.session.query(CurvePool)
+        .with_entities(CurvePool.address, CurvePool.name)
         .filter(CurvePool.chain == chain)
         .all()
     )
-    return CurvePoolNameSchema(many=True, session=db.session).load(
-        result, unknown=EXCLUDE
-    )
+
+    result = [CurvePoolNameSchema().load(row._asdict()) for row in result]
+
+    return result
 
 
 def get_all_pool_metadata(chain: str) -> List[CurvePool]:
-    result = db.query(CurvePool).filter(CurvePool.chain == chain).all()
+    result = db.session.query(CurvePool).filter(CurvePool.chain == chain).all()
     return CurvePoolSchema(many=True, session=db.session).load(
         result, unknown=EXCLUDE
     )
@@ -38,7 +45,7 @@ def get_all_pool_metadata(chain: str) -> List[CurvePool]:
 
 def get_pool_metadata(chain: str, pool: str) -> List[CurvePool]:
     result = (
-        db.query(CurvePool)
+        db.session.query(CurvePool)
         .filter(CurvePool.id == f"{pool.lower()}-{chain}")
         .all()
     )
