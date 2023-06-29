286727733fd3ed17019901998f638f30032bbc4d
benber86 <benoitb@tutanota.de>
2023-06-30 00:40:26 +1000

    Add StakeDAO crvUSD yields

--- app/services/curve/yields.py
+++ app/services/curve/yields.py
@@ -2,10 +2,12 @@ from operator import and_
 from typing import List
 import requests
 from sqlalchemy import func
-
+import logging
 from main import db
 from models.convex.snapshot import ConvexPoolSnapshot
 from models.curve.crvusd import CrvUsdYield
+
+logger = logging.getLogger(__name__)
 
 
 def get_latest_convex_pool_apr() -> List:
@@ -78,8 +80,40 @@ def get_conic_omnipool() -> List[CrvUsdYield]:
     ]
 
 
+def get_std_yields() -> List[CrvUsdYield]:
+    STD_YIELD = "https://lockers.stakedao.org/api/strategies/cache/curve"
+    r = requests.get(STD_YIELD)
+    yields = {
+        a["name"]: sum([b["apr"] for b in a["aprBreakdown"]]) * 100
+        for a in r.json()
+        if "crvusd" in a["key"]
+    }
+    return [
+        CrvUsdYield(platform="StakeDAO", pool=k, apy=v)
+        for k, v in yields.items()
+    ]
+
+
 def get_crv_usd_yields() -> List[CrvUsdYield]:
-    convex_yields = get_latest_convex_pool_apr()
-    curve_yields = get_max_boost_curve_yield()
-    conic_yields = get_conic_omnipool()
-    return conic_yields + convex_yields + curve_yields
+    try:
+        convex_yields = get_latest_convex_pool_apr()
+    except Exception as e:
+        logger.error(f"Error fetching Convex yields : {e}")
+        convex_yields = []
+    try:
+        curve_yields = get_max_boost_curve_yield()
+    except Exception as e:
+        logger.error(f"Error fetching Curve yields : {e}")
+        curve_yields = []
+    try:
+        conic_yields = get_conic_omnipool()
+    except Exception as e:
+        logger.error(f"Error fetching Conic yields : {e}")
+        conic_yields = []
+    try:
+        std_yields = get_std_yields()
+    except Exception as e:
+        logger.error(f"Error fetching StakeDAO yields : {e}")
+        std_yields = []
+
+    return conic_yields + convex_yields + curve_yields + std_yields
