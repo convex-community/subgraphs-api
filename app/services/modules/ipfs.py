fb4ea9f16ef4cda2283623ca707b49953174e831
benber86 <benoitb@tutanota.de>
2023-12-05 05:22:09 +1100

    Handle time out error

--- app/services/modules/ipfs.py
+++ app/services/modules/ipfs.py
@@ -1,15 +1,23 @@
 from main.const import INFURA_API_KEY, INFURA_SECRET_KEY
 from requests.auth import HTTPBasicAuth
 import requests
+import logging
 import json
+
+logger = logging.getLogger()
 
 
 def retrieve_proposal_text_from_ipfs(ipfs_hash: str):
     parsed_hash = ipfs_hash.split(":")[-1]
     url = f"https://ipfs.infura.io:5001/api/v0/cat?arg={parsed_hash}"
-    response = requests.post(
-        url, auth=HTTPBasicAuth(INFURA_API_KEY, INFURA_SECRET_KEY), timeout=20
-    )
+    try:
+        response = requests.post(
+            url,
+            auth=HTTPBasicAuth(INFURA_API_KEY, INFURA_SECRET_KEY),
+            timeout=20,
+        )
+    except requests.exceptions.ReadTimeout as e:
+        logger.warning(f"IPFS retrieval failed due to timeout: {e}")
     if response:
         data = json.loads(response.text)
         return data["text"]
